7a5ddcf3a2abb3d6457858dc07cf6d08908ad530
Hamrozjon Kosimov <141737711+Hamroz-JONN@users.noreply.github.com>
2024-10-05 18:48:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 Hamroz's Forecast")
 
 st.write('Predict your apartment price')
+
+with st.expander('Initial data'):
+  st.write("**Raw Data**")
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  st.write("**X**")
+  x_raw = df.drop('species', axis=1)
+  x_raw
+
+  st.write("**Y**")
+  y_raw = df.species
+  y_raw
