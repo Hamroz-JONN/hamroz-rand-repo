8c139eb785b5718596ae46316b8b562aaaa8d762
Hamrozjon Kosimov <141737711+Hamroz-JONN@users.noreply.github.com>
2024-10-05 18:59:29 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.title("🎈 Hamroz's Forecast")
+st.title('😁😁😁  First App Abubakr')
 
-st.write('Predict your apartment price')
+st.write('Hello world!')
 
 with st.expander('Initial data'):
-  st.write("**Raw Data**")
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-  st.write("**X**")
-  x_raw = df.drop('species', axis=1)
-  x_raw
+  
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
 
-  st.write("**Y**")
+  st.write('**y**')
   y_raw = df.species
   y_raw
 
-  df.columns
-
-  with st.expander('Data Viz'):
-      st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
